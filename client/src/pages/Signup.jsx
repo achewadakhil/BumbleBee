2aafa0cadbbf21a24bf778ca9412150cf234783b
Akhil <achewadakhil11@gmail.com>
2025-12-09 01:27:53 +0530

    Initial client page

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+export default function Signup(){
+    return <>
+        <div className="h-screen bg-gray-500 flex justify-center items-center">
+            <div className="h-[30vh] w-[30vw] bg-red-500">
+                <h1></h1>
+            </div>
+        </div>
+    </>
+}
