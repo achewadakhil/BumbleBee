56e77171197249ebf9163178c80808ecdc3d7014
Akhil <achewadakhil11@gmail.com>
2025-12-09 23:32:19 +0530

    Basic home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import SigninPage from "./pages/Signin";
 import SignupPage from "./pages/Signup";
+import Home from "./pages/Home";
+
+function Layout(){
+  return <div className="h-screen bg-gray-900">
+    <Outlet />
+  </div>
+}
 
 export default function App(){
 
   return <>
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element = {<SignupPage />} />
-        <Route path="/signin" element = {<SigninPage />} />
+          <Route element = {<Layout />}>
+            <Route path = "/" element = { <Home />} />
+            <Route path="/signup" element = {<SignupPage />} />
+            <Route path="/signin" element = {<SigninPage />} />
+          </Route>
       </Routes>
     </BrowserRouter>
   </>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center text-white px-4">
+      
+      <h1 className="text-5xl font-extrabold mb-8 text-center">
+        BumbleBee
+      </h1>
+
+      <div className="flex gap-6">
+        <Link
+          to="/signup"
+          className="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-xl text-lg font-semibold transition"
+        >
+          Sign Up
+        </Link>
+
+        <Link
+          to="/signin"
+          className="bg-green-600 hover:bg-green-700 px-6 py-3 rounded-xl text-lg font-semibold transition"
+        >
+          Sign In
+        </Link>
+      </div>
+    </div>
+  );
+}
